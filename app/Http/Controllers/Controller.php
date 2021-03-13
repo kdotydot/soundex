d908b6918d707655c2d8f6da225205d6b37712c0
Kyron Joves <kyron.joves@gmail.com>
2021-03-13 12:29:25 +0800

    initial commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,34 +13,31 @@ class Controller extends BaseController
     {
 
         $data = $request->all();
-        $lastname_list =  DB::table('profiles')->select('*')->get();
-        $firstname_list =  DB::table('profiles')->select('profile_firstname')->get();
-        $middlename_list =  DB::table('profiles')->select('profile_middlename')->get();
-        $match = DB::table('profiles')->select('profile_lastname', 'profile_firstname')
+        $list =  DB::table('profiles')->select('*')->get();
+        $match = DB::table('profiles')
+        ->select('profile_lastname', 'profile_firstname')
         ->where('profile_lastname', '=', $request->lastname)
         ->where('profile_firstname', '=', $request->firstname)
         ->get();
         $array = array();
-
+        $array2 = array();
         if($match->isEmpty())
         {
-            $soundex = DB::table('profiles')->select('*')
-                ->where('profile_lastname', '==', soundex($request->lastname))
-                ->get();
-                // dd($soundex);
-            foreach($lastname_list as $lastname)
+            foreach($list as $name)
             {
-                // echo $lastname->profile_lastname;
-                if (soundex($lastname->profile_lastname) == soundex($request->lastname)){
+                if (soundex($name->profile_lastname) == soundex($request->lastname))
+                {
                 //    return "Do you mean " . $lastname->profile_lastname . "?";
-                    array_push($array, $lastname);
-
-
+                    array_push($array, $name);
+                }
+                if (soundex($name->profile_firstname) == soundex($request->firstname))
+                {
+                    //    return "Do you mean " . $lastname->profile_lastname . "?";
+                        array_push($array2, $name);
                 }
 
             }
-            return response()->json(['code' => '201', 'status' => 'successfull', 'message' => 'Do you mean this?', 'data' => $array]);
-            // return response()->json(['code' => '201', 'status' => 'successfull', 'data' => 'zero result']);
+            return response()->json(['code' => '201', 'status' => 'successfull', 'message' => 'Do you mean this?', 'lastname' => $array, 'firstname' => $array2]);
 
         }
         else
